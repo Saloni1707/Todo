c2396b7307c1c9a42050b46aeced4a4ba136b6bd
Saloni Atole <145243379+Saloni1707@users.noreply.github.com>
2024-09-15 22:20:39 +0530

    Add files via upload
    Here there are different ways to do CRUD operations on todo !!!

---
+++ index.js
@@ -0,0 +1,22 @@
+// initialise empty package.json and for any node project using npm init -y
+//patch basically updates the files 
+
+const express = require("express")
+const app = express()
+//route handlers 
+app.get('/',function(req,res){
+    //gives all the information regarding the request 
+    res.send("hello world")
+})
+
+app.get('/asd',function(req,res){
+    res.json({
+        "name":saloni ,
+        "age":18,
+    })
+})
+
+app.listen(3000) //which port to listen on ->listening infinitely on this port 
+
+//store the data in a file , foundation for file system 
+//add user logic to it as well 
